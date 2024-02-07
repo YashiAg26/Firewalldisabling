e4dd4d4024a9cfff5dd0d6cf3a5416c7ae40ac4e
YashiAg26 <92780959+YashiAg26@users.noreply.github.com>
2024-02-07 16:56:16 +0530

    Update providers.tf

--- Azure-VM-Windows1/providers.tf
+++ Azure-VM-Windows1/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "azurerm" {
-    client_id       = "0402db4e-72b8-4dfa-b13b-34fe262b2f77"
+    client_id       = "72ba6eba-b9f7-4d4a-97f5-3c8ce354f1be"
     client_secret   = "CS_8Q~hN1R-EL0xZEXNR7TCYNoqOrJI2eEoGda8q"
     tenant_id       = "664f4fb1-8e7a-471d-b7cc-1344e98849e5"
     subscription_id = "5beddca6-6c7a-426d-a704-8305f3fb07ab"
