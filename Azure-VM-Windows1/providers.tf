6dce4884dafc9b5c2a5bc71331a067da2c9c77db
YashiAg26 <92780959+YashiAg26@users.noreply.github.com>
2024-02-07 17:10:03 +0530

    Update providers.tf

--- Azure-VM-Windows1/providers.tf
+++ Azure-VM-Windows1/providers.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }    
-  }
-}
-
 provider "azurerm" {
-    client_id       = "72ba6eba-b9f7-4d4a-97f5-3c8ce354f1be"
-    client_secret   = "CS_8Q~hN1R-EL0xZEXNR7TCYNoqOrJI2eEoGda8q"
-    tenant_id       = "664f4fb1-8e7a-471d-b7cc-1344e98849e5"
-    subscription_id = "5beddca6-6c7a-426d-a704-8305f3fb07ab"
+    version = "~>2.0"
     features { }
-    skip_provider_registration = true
 }
